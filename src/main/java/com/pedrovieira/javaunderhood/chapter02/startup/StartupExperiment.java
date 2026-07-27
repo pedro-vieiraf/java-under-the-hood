ef1afadb77f60f0e6312d59acd8268f8cdf9e334
PedroVieira <pedrovieirafdev@gmail.com>
2026-07-27 10:18:25 -0300

    feat: Experiment 01

--- src/main/java/com/pedrovieira/javaunderhood/chapter02/startup/StartupExperiment.java
+++
@@ -1,7 +0,0 @@
-package com.pedrovieira.javaunderhood.chapter02.startup;
-
-public class StartupExperiment {
-    public static void main(String[] args) {
-        System.out.println("Initial Commit.");
-    }
-}
---
+++ src/main/java/com/pedrovieira/javaunderhood/chapters/chapter02/startup/compilation/SimpleApplication.java
@@ -0,0 +1,8 @@
+package com.pedrovieira.javaunderhood.chapters.chapter02.startup.compilation;
+
+public class SimpleApplication {
+
+    public static void main(String[] args) {
+        System.out.println("Hello from Java bytecode!");
+    }
+}
