8d673399700eda81718ef328bf38b191f64b0ce1
PedroVieira <pedrovieirafdev@gmail.com>
2026-07-27 09:19:39 -0300

    initial commit

---
+++ src/main/java/com/pedrovieira/javaunderhood/chapter02/startup/StartupExperiment.java
@@ -0,0 +1,7 @@
+package com.pedrovieira.javaunderhood.chapter02.startup;
+
+public class StartupExperiment {
+    public static void main(String[] args) {
+        System.out.println("Initial Commit.");
+    }
+}
